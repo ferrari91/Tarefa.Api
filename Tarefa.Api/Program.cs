af30ed9d21c349b095a63a5c5fbba4c2305cdaae
Fernando Ferrari <148151553+ferrarifrwk@users.noreply.github.com>
2023-12-12 16:38:38 -0300

    Injeção de dependência do repository.

--- Tarefa.Api/Program.cs
+++ Tarefa.Api/Program.cs
@@ -1,10 +1,13 @@
 using Tarefa.Infrastructure.DataBase;
+using Tarefa.Infrastructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.ConfigureDataBase(builder.Configuration);
+builder.Services.ConfigureResporitories();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ Tarefa.Infrastructure/Repository/Setup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Tarefa.Application.Interface.Repositories;
+
+namespace Tarefa.Infrastructure.Repository
+{
+    public static class Setup
+    {
+        public static void ConfigureResporitories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ITaskRepository, TaskRepository>();
+        }
+    }
+}
